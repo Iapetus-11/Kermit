fca58d84bf0e1dfb7f1af9100fc72f065a182336
Milo Weinberg <iapetus011@gmail.com>
2021-08-10 13:27:00 -0400

    add startup event

--- src/kermit.nim
+++ src/kermit.nim
@@ -4,18 +4,23 @@ import prologue
 import json
 
 let config = parseJson(readFile("config.json"))
-let appSettings = newSettings(
-  appName="Kermit Bot",
-  address=config["address"].getStr(),
-  port=Port(config["port"].getInt()),
-  debug=config["debug"].getBool()
-)
 
-let app = newApp(appSettings)
 let http = newAsyncHttpClient()
+
+proc startup() {.gcsafe, async.} =
+  echo "Kermit started! Webhook listening on ", config["address"].getStr(), ":", config["port"].getInt(), "!"
 
 proc setupInteractions() {.async.} =
   discard
+
+let appSettings = newSettings(
+  appName = "Kermit Bot",
+  address = config["address"].getStr(),
+  port = Port(config["port"].getInt()),
+  debug = config["debug"].getBool()
+)
+
+let app = newApp(appSettings, startup = @[initEvent(startup)])
 
 proc interaction(ctx: Context) {.async, gcsafe.} =
   # verify the request
